dfe2a26e72e4ae1cb745fcd6f9f3d44eb93c5c21
Heinz Der Flugel <heinzdf@gabuterscape.my.id>
2021-08-29 17:41:01 +0700

    Revert "sdm710-common: clone pe telephony repos"
    This reverts commit 6d377d33ca9ab7cd38731069f3e69e062cd01b3e.

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -8,8 +8,3 @@ git clone https://github.com/Gabuters-Dev/gabuters-clang $gclang
 else
 echo "Gabuters clang exist, skipping.."
 fi
-
-### Using PE Telephony Repos ###
-rm -rf frameworks/opt/telephony/ && git clone https://github.com/PixelExperience/frameworks_opt_telephony.git frameworks/opt/telephony/
-rm -rf packages/services/Telephony/ && git clone https://github.com/PixelExperience/packages_services_Telephony.git packages/services/Telephony/
-rm -rf vendor/codeaurora/telephony/ && git clone https://github.com/PixelExperience/vendor_codeaurora_telephony.git vendor/codeaurora/telephony/
