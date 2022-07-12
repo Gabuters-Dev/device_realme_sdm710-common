094c1567d0b0171c288bdeff181432e4a5f2e98c
arco <>
2022-07-12 19:06:01 +0530

    sdm710-common: Silence clang warnings during kernel compile
    clang-11: warning: argument unused during compilation:
    '-fuse-ld=lld' [-Wunused-command-line-argument]
    
    Signed-off-by: kssrao13882 <kssrao13882@gmail.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -50,6 +50,8 @@ BOARD_KERNEL_CMDLINE += androidboot.init_fatal_reboot_target=recovery
 BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
 BOARD_KERNEL_PAGESIZE := 4096
 BOARD_KERNEL_SEPARATED_DTBO := true
+TARGET_KERNEL_ADDITIONAL_FLAGS := \
+    HOSTCFLAGS="-fuse-ld=lld -Wno-unused-command-line-argument"
 TARGET_KERNEL_ARCH := arm64
 TARGET_KERNEL_SOURCE := kernel/realme/RMX1851
 TARGET_COMPILE_WITH_MSM_KERNEL  := true
