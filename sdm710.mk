e8b9a5e54e16038a25e0ea95e83f4a0f39ef9b83
alk3pInjection <webmaster@raspii.tech>
2022-10-22 10:45:26 +0530

    sdm710-common: configs: Tweak msm_irqbalance We should ignore latency sensitive IRQs from userspace IRQ balancing.
    Signed-off-by: alk3pInjection <webmaster@raspii.tech>
    Change-Id: Ia82c602ae45592e74470d6067f97f56b2248f24d
    Signed-off-by: Dmitrii <bankersenator@gmail.com>

--- sdm710.mk
+++ sdm710.mk
@@ -310,6 +310,10 @@ PRODUCT_PACKAGES += \
     android.hardware.tetheroffload.control@1.0 \
     android.hardware.tetheroffload.config@1.0
 
+# IRQBalance
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/msm_irqbalance.conf:$(TARGET_COPY_OUT_VENDOR)/etc/msm_irqbalance.conf
+
 # IPC router config
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/sec_config:$(TARGET_COPY_OUT_VENDOR)/etc/sec_config
