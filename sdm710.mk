43f374fc5ffac9842372a5f1eba006c0682f6547
Heinz Der Flugel <heinzdf@gabuterscape.my.id>
2022-10-30 14:23:46 +0700

    Revert "sdm710-common: Build extphone libraries as required by Android 12 telephony"
    This reverts commit f6c9584c5307f543db8fc4c174a4fb658cc6e15f.

--- sdm710.mk
+++ sdm710.mk
@@ -427,10 +427,6 @@ PRODUCT_SOONG_NAMESPACES += \
 
 # Telephony
 PRODUCT_PACKAGES += \
-    extphonelib \
-    extphonelib-product \
-    extphonelib.xml \
-    extphonelib_product.xml \
     telephony-ext \
     qti-telephony-hidl-wrapper \
     qti_telephony_hidl_wrapper.xml \
