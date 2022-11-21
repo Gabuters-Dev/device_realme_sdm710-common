97346541bc95feb1de1f0d9428fc212923e9b646
Heinz Der Flugel <heinzdf@gabuterscape.my.id>
2022-11-21 19:17:11 +0700

    sdm710-common: Import device framework matrix from generated blobs

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -111,6 +111,7 @@ USE_DEVICE_SPECIFIC_GPS := true
 
 # HIDL
 DEVICE_FRAMEWORK_MANIFEST_FILE := $(COMMON_PATH)/framework_manifest.xml
+DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := $(COMMON_PATH)/device_framework_matrix.xml
 DEVICE_MATRIX_FILE := $(COMMON_PATH)/compatibility_matrix.xml
 DEVICE_MANIFEST_FILE := $(COMMON_PATH)/manifest.xml
 
