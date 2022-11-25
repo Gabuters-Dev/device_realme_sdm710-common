8169f241072feae62fc7fb62f529609e1fa99f6d
Heinz Der Flugel <heinzdf@gabuterscape.my.id>
2022-11-26 01:48:34 +0700

    Revert "sdm710-common: Let's Ship Lineage Aperture Camera"
    This reverts commit ef8251d371cff65b863daffe4428ef236fd9a2e5.

--- sdm710.mk
+++ sdm710.mk
@@ -155,7 +155,6 @@ PRODUCT_PACKAGES += \
     android.frameworks.displayservice@1.0 \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service_64 \
-    ApertureCam \
     libxml2
 
 # CNE
