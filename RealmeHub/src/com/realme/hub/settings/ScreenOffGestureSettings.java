aecd638a08e380f9fafc23f6e8744c480efda2be
Nishant Kumar <www.rajsonu13@gmail.com>
2022-12-09 18:53:59 +0530

    sdm710-common: RealmeHub: Fix Compilation error after Android-13.0.0_r18 QPR1 merge
    * Package name changed here https://android.googlesource.com/platform/frameworks/base/+/refs/tags/android-13.0.0_r18/packages/SettingsLib/CollapsingToolbarBaseActivity/AndroidManifest.xml
    Signed-off-by: Nishant Kumar <www.rajsonu13@gmail.com>
    Signed-off-by: kssrao13882 <kssrao13882@gmail.com>

--- RealmeHub/src/com/realme/hub/settings/ScreenOffGestureSettings.java
+++ RealmeHub/src/com/realme/hub/settings/ScreenOffGestureSettings.java
@@ -19,7 +19,7 @@ package com.realme.hub.settings;
 import android.os.Bundle;
 import android.view.MenuItem;
 import com.android.settingslib.collapsingtoolbar.CollapsingToolbarBaseActivity;
-import com.android.settingslib.collapsingtoolbar.R;
+import com.android.settingslib.widget.R;
 
 public class ScreenOffGestureSettings extends CollapsingToolbarBaseActivity {
 
