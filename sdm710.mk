0366abe2a7aea81ee4a394b37b87c98627b4f3a0
Heinz Der Flugel <heinzdf@gabuterscape.my.id>
2022-12-29 00:34:18 +0700

    Revert "sdm710-common: Nuke GcamGo"
    This reverts commit e136b9f8424af869aea3699457175a249e0548c6.

--- sdm710.mk
+++ sdm710.mk
@@ -155,7 +155,8 @@ PRODUCT_PACKAGES += \
     android.frameworks.displayservice@1.0 \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service_64 \
-    libxml2
+    libxml2 \
+    GcamGo
 
 # CNE
 PRODUCT_PACKAGES += \
